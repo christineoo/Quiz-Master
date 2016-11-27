d8ef6cca9514c630f2e2ea92afd897a09871df0b
Christine Oo <ootjinvoon@gmail.com>
2016-11-28 02:03:19 +0800

    Refactor tests

--- src/app/test/components/DeleteQuestion.spec.js
+++ src/app/test/components/DeleteQuestion.spec.js
@@ -1,49 +1,66 @@
 import expect from 'expect';
 import jsdom from 'mocha-jsdom'
 import React from 'react'
+import sinon from 'sinon';
+import { mount } from 'enzyme';
 import TestUtils from 'react-addons-test-utils';
 import expectJSX from'expect-jsx';
 import DeleteQuestion from '../../components/question/DeleteQuestion';
 import { Dialog, FlatButton } from 'material-ui';
+import { RaisedButton } from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 expect.extend(expectJSX);
 describe('Delete Question Component', () => {
     jsdom();
 
-    it('should render question passed into this component', () => {
-        const question = {
-            id: 1,
-            answer: "26",
-            content: "<p>How many letters are there in the <code><strong>English</strong></code> alphabet?</p>",
+    const handleOnDeleteDialogClose = sinon.spy();
+    const handleOnSubmitClick = sinon.spy();
+    const question = {
+        id: 1,
+        answer: "26",
+        content: "<p>How many letters are there in the <code><strong>English</strong></code> alphabet?</p>",
+    };
+    let setup = () => {
+        const muiTheme = getMuiTheme();
+
+        const props = {
+            selectedQuestion: question,
+            openDeleteDialog: false,
+            onDeleteDialogClose: handleOnDeleteDialogClose,
+            onSubmitClick: handleOnSubmitClick
         };
-        function handleDeleteSubmit(){
-            return null;
-        }
-        function handleDeleteDialogClose() {
-            return null;
-        }
+
+        return mount(<DeleteQuestion {...props} />, {
+            context: {muiTheme},
+            childContextTypes: {muiTheme: React.PropTypes.object}
+        });
+    };
+
+    it('should render question passed into Delete Question component', () => {
         const renderer = TestUtils.createRenderer();
         renderer.render(<DeleteQuestion selectedQuestion={question}
                                         openDeleteDialog={true}
-                                        onSubmitClick={handleDeleteSubmit}
-                                        onDeleteDialogClose={handleDeleteDialogClose}
+                                        onSubmitClick={sinon.spy()}
+                                        onDeleteDialogClose={sinon.spy()}
         />);
         const actual = renderer.getRenderOutput();
         const expected = (
             <Dialog
                 title="Are you sure you want to delete this question?"
                 open={true}
-                onRequestClose={handleDeleteDialogClose}
+                onRequestClose={handleOnDeleteDialogClose}
                 modal={true}
                 actions={[<FlatButton
                     label={'Cancel'}
                     secondary={true}
-                    onTouchTap={handleDeleteDialogClose} />,
+                    onTouchTap={handleOnDeleteDialogClose} />,
                     <FlatButton
                     label={'Delete'}
                     primary={true}
                     disabled={false}
-                    onTouchTap={handleDeleteSubmit} />]}
+                    onTouchTap={handleOnSubmitClick} />]}
                 autoScrollBodyContent={true}
             >
                 <p>Question: </p>
---
+++ src/app/test/components/Home.spec.js
@@ -0,0 +1,91 @@
+import expect from 'expect';
+import React from 'react'
+import TestUtils from 'react-addons-test-utils';
+import expectJSX from'expect-jsx';
+import { mount, shallow } from 'enzyme';
+import jsdom from 'mocha-jsdom';
+import sinon from 'sinon';
+import { RaisedButton } from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Home from '../../components/home/Home';
+
+injectTapEventPlugin();
+expect.extend(expectJSX);
+
+describe('Home Component', () => {
+    jsdom();
+
+    const handleQuizModeClick = sinon.spy();
+    const handleManageQuestionModeClick = sinon.spy();
+    let setup = () => {
+        const muiTheme = getMuiTheme();
+
+        const props = {
+            onQuizModeClick: handleQuizModeClick,
+            onManageQuestionModeClick: handleManageQuestionModeClick
+        };
+
+        return mount(<Home {...props} />, {
+            context: {muiTheme},
+            childContextTypes: {muiTheme: React.PropTypes.object}
+        });
+    };
+
+    it('should render home Manage Questions Mode button and Quiz Mode button', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Home onQuizModeClick={handleQuizModeClick}
+                              onManageQuestionModeClick={handleManageQuestionModeClick} />);
+        const actual = renderer.getRenderOutput();
+        const expected = (
+            <div className="container">
+                <RaisedButton
+                    className="button"
+                    label="Manage Questions Mode"
+                    primary={true}
+                    onTouchTap={handleManageQuestionModeClick}
+                />
+                < RaisedButton
+                    className="button"
+                    label = "Quiz Mode"
+                    primary = {true}
+                    onTouchTap={handleQuizModeClick}
+                />
+            </div>
+        );
+        expect(actual).toIncludeJSX(expected);
+    });
+
+    it('should render buttons with label Manage Questions Mode and Quiz Mode', () => {
+        const expectedManageQuestionModeButtonLabel = "Manage Questions Mode";
+        const expectedQuizModeButtonLabel = "Quiz Mode";
+        const wrapper = setup();
+        let buttons = TestUtils.scryRenderedDOMComponentsWithTag(
+            wrapper.instance(), 'button'
+        );
+
+        const actualManageQuestionModeButton = buttons[0].textContent;
+        const actualQuizModeButtonLabel = buttons[1].textContent;
+
+        expect(actualManageQuestionModeButton).toEqual(expectedManageQuestionModeButtonLabel);
+        expect(actualQuizModeButtonLabel).toEqual(expectedQuizModeButtonLabel);
+    });
+
+    it('should click Manage Questions Mode once', () => {
+        const wrapper = setup();
+        let buttons = TestUtils.scryRenderedDOMComponentsWithTag(
+            wrapper.instance(), 'button'
+        );
+        TestUtils.Simulate.touchTap(buttons[0]);
+        sinon.assert.calledOnce(handleManageQuestionModeClick);
+    });
+
+    it('should click Quiz Mode once', () => {
+        const wrapper = setup();
+        const buttons = TestUtils.scryRenderedDOMComponentsWithTag(
+                wrapper.instance(), 'button'
+            );
+        TestUtils.Simulate.touchTap(buttons[1]);
+        sinon.assert.calledOnce(handleQuizModeClick);
+    });
+});
